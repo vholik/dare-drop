edf1520794365a5fdda7821a79d1d5644706d2cb
vholik <1viktorholik@gmail.com>
2023-06-29 22:16:54 +0200

    adding email check to dto

--- dare-drop-server/src/users/dtos/create-user.dto.ts
+++ dare-drop-server/src/users/dtos/create-user.dto.ts
@@ -1,10 +1,16 @@
-import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
-  @IsString()
   @MinLength(3)
   @MaxLength(50)
+  @IsEmail()
   email: string;
 
   @IsNotEmpty()
